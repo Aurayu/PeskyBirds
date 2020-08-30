f3e0c602fd85189dc23e673d59a5b7eaee4c9cfc
Joshua Finch <me@joshuafinch.com>
2020-08-30 16:47:50 +0100

    Update icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Pesky Birds`,
+    description: `We're a whitelisted community server that like to play semi-vanilla Minecraft on Java 1.16.2`,
+    author: `Aurayu, LCFinch`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,13 +18,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Pesky Birds`,
+        short_name: `Pesky Birds`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        background_color: `#DD123E`,
+        theme_color: `#DD123E`,
+        display: `standalone`,
+        icon: `src/images/pb-square-icon.svg`, // This path is relative to the root of the site.
+        legacy: false,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
